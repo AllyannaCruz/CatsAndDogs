e4ff9aefb95e7c09b6e2c9f0a46ee193e01abee9
AllyannaCruz <55791523+AllyannaCruz@users.noreply.github.com>
2020-02-10 12:14:57 +0000

    more dogs than cats

--- Animal.java
+++ Animal.java
@@ -5,9 +5,15 @@ public class Animal
     //local to class
     private String name; // make two public accessors
 
+    public Animal(String num)
+    {
+       setName(name);
+    }
+
+
     public String getName() //accessor for name must be spelled getName
     {
-        setName(name);
+        
     }
 
     public void getName(String name)//accessor method
@@ -18,6 +24,13 @@ public class Animal
     //constructor - object is called when u create an object --default constructor when not made
     //constructor must be the same as CLass
 
-    public void setName()
+    public void setName(){
+
+    }
+
+    public void speak()
+    {
+        System.out.println("Meow!");
+    }
 
 }
--- Main.java
+++ Main.java
@@ -5,5 +5,22 @@ public class Main{
 }
 
 public static void main(String[] args){
-    
+
 }
+//This is not polymorphism
+Cat topcat = new Cat("Topcat");
+
+//This is polymorphism
+Animal mino = new Cat("Mino");
+
+//a virtual method
+//dynamic binding  -- //always call one of instance instead of super
+mino.speak(); // Cat method speak gets called
+
+//cast -- (Cat) added
+System.out.println((Cat)mino.getNumLives()); //program won't compile here // compiler wont know mino is a cat, only knows its an animal
+//compiler will only know when you run the program.
+
+mino = new Dog("Misty"); // This will compile because type is Animal
+topcat = new Dog("Tara");// Won't compile because type is cat
+
